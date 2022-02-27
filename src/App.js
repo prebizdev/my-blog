0b43827525a3c4062ed094fb3573c42ba17432b8
Krutarth Patel <5004048+karmadice@users.noreply.github.com>
2022-02-27 02:33:10 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage.js';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <>
+        <h1>HomePage</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam a, hic ratione dolorum debitis nesciunt ex non natus dignissimos aspernatur laboriosam voluptate officia, voluptatibus ab tempora aliquam labore expedita! Fuga!</p>
+        <p>Corporis unde expedita quasi et rem magnam omnis non veritatis iste enim reprehenderit quia, commodi nobis tenetur atque odio? Omnis illum consectetur dolor debitis aut! Reprehenderit sequi voluptatum eaque excepturi!</p>
+        <p>Sint, vero. Aspernatur, earum, repudiandae sit vero corrupti fugiat ullam ipsa nihil praesentium dolorem adipisci dolores at ratione architecto voluptas deserunt quibusdam temporibus? Consequatur, saepe amet corporis maxime expedita ipsam.</p>
+        <p>Saepe dolor corrupti aspernatur necessitatibus illo ipsam, exercitationem dolorem, ratione accusantium tempore optio sapiente sit modi sed neque! Vitae facere maxime reprehenderit numquam velit ipsum eaque ipsa dolor unde aliquid?</p>
+        <p>Similique in ipsum fuga. Perspiciatis dolores, autem ratione soluta animi atque voluptate obcaecati est quia, dolorum dolor? Provident, voluptas molestias. Rerum nam atque beatae! Quibusdam adipisci nisi velit minima cumque.</p>
+    </>
+  )
+}
+
+export default HomePage
